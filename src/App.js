1006736d5214dd1910e12d4b0e55671a8c42f3c1
milleseseau <leojimlavi@gmail.com>
2021-12-16 23:54:57 -0500

    User display and update page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Home from "./pages/home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
 
 function App() {
 	return (
@@ -29,6 +30,18 @@ function App() {
 							<div className="container">
 								<Sidebar />
 								<UserList />
+							</div>
+						</>
+					}
+				></Route>
+				<Route
+					path="/user/:userId"
+					element={
+						<>
+							<Topbar />
+							<div className="container">
+								<Sidebar />
+								<User />
 							</div>
 						</>
 					}
